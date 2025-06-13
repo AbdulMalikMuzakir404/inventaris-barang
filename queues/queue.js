e81d1ee247e1e0b7639af9ff10afe27ca34a8351
TheDoctor <abdulmalikmuzakir55@gmail.com>
2025-06-14 00:24:25 +0700

    Update Redis configuration in queue.js to include password and TLS settings for enhanced security.

--- queues/queue.js
+++ queues/queue.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const redisOptions = {
   host: process.env.REDIS_HOST || "127.0.0.1",
   port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD || undefined,
+  tls: {}
 };
 
 const exportQueue = new Bull("export-items", { redis: redisOptions });
